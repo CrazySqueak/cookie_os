652ddc0793d7f6c5e050feb3f66953c45c3f252f
Its.Annie <ctrlaltcs@gmail.com>
2024-07-12 09:55:03 +0100

    Attempted to add a proper way of adding memory blocks or something

--- kernel/rust/src/memory/physical.rs
+++ kernel/rust/src/memory/physical.rs
@@ -49,6 +49,7 @@ impl !Sync for PhysicalMemoryAllocation{}
 
 // ALLOCATOR
 // This allocator works akin to a buddy allocator or something
+#[derive(Debug)]
 #[repr(transparent)]
 pub struct BuddyAllocator<const MAX_ORDER: usize, const MIN_SIZE: usize> {
     free_blocks: [Vec<usize>; MAX_ORDER],
@@ -61,7 +62,7 @@ impl<const MAX_ORDER: usize, const MIN_SIZE: usize> BuddyAllocator<MAX_ORDER,MIN
     }
     
     /* Get the memory addresses of the memory address provided and its buddy, as a tuple of (lower buddy addr, higher buddy addr).
-    One of these will be the memory address provided. The other will be its buddy.*/
+    One of these will be the memory address provided (if it is a valid address of a block). The other will be its buddy.*/
     pub const fn buddies(order: usize, addr: usize) -> (usize, usize) {
         let low_addr = addr &!(Self::block_size(order)<<1);  // we can't call block_size(order+1) as there's no guarantee that order is not MAX_ORDER
         let high_addr = low_addr + Self::block_size(order);
@@ -115,35 +116,66 @@ impl<const MAX_ORDER: usize, const MIN_SIZE: usize> BuddyAllocator<MAX_ORDER,MIN
         
         Some(to_addr)
     }
+    
+    /* Add memory from [start,end) to this allocator's list of free blocks. */
+    pub unsafe fn add_memory(&mut self, start: *const u8, end: *const u8){
+        for order in (1..MAX_ORDER+1).rev() {  // MAX_ORDER -> 1 inc
+            // Calculate the bounds of the block
+            let split_order = order-1;
+            let (block_start_addr, block_mid_addr) = Self::buddies(split_order, start as usize);
+            let block_end_excl = block_mid_addr + Self::block_size(split_order);
+            if block_start_addr < (start as usize) || block_end_excl > (end as usize) { continue; }  // block is too big / overlaps the bounds
+            
+            if (start as usize) < block_start_addr {
+                // We need to add the extra area at the start (that the block doesn't cover)
+                self.add_memory(start, block_start_addr as *const u8);
+            }
+            if (end as usize) > block_end_excl {
+                // We need to add the extra area at the end
+                self.add_memory(block_end_excl as *const u8, end);
+            }
+            
+            // Add the block
+            // (the low buddy is guaranteed to also count as a valid block for the order above)
+            self.free_blocks[order].push(block_start_addr);
+            return; // and return
+        }
+    }
 }
-mutex_no_interrupts!(LockedFrameAllocator, BuddyAllocator<27,4096>);
-
+mutex_no_interrupts!(LockedPFrameAllocator, BuddyAllocator<27,4096>);
+lazy_static! {
+    static ref PHYSMEM_ALLOCATOR: LockedPFrameAllocator = LockedPFrameAllocator::wraps(BuddyAllocator {
+        free_blocks: core::array::from_fn(|_| Vec::new())
+    });
+}
 
 pub fn init_pmem(mmap: &Vec<MemoryMapEntry>){
     let (_, kend) = get_kernel_bounds();  // note: we ignore any memory before the kernel, its a tiny sliver (2MB tops) and isn't worth it
     write!(SERIAL1, "\n\tKernel ends @ {:x}", kend);
-    //without_interrupts(||{
-    //    let mut allocator = PHYSMEM_ALLOCATOR.lock();
-    //    for entry in mmap {
-    //        write!(SERIAL1, "\nChecking PMem entry {:?}", entry);
-    //        unsafe {
-    //            if !entry.is_for_general_use() { continue; }
-    //            write!(SERIAL1, "\n\tEntry is for general use.");
-    //            let start_addr: usize = entry.base_addr.try_into().unwrap();
-    //            let end_addr: usize = (entry.base_addr + entry.length).try_into().unwrap();
-    //            write!(SERIAL1, "\n\tRange: [{:x},{:x})", start_addr, end_addr);
-    //            if start_addr >= kend {
-    //                // after the kernel
-    //                write!(SERIAL1, "\n\tAfter the kernel");
-    //                allocator.add_to_heap(start_addr, end_addr);
-    //            } else if end_addr > kend {
-    //                // intersecting the kernel
-    //                write!(SERIAL1, "\n\tIntersects the kernel");
-    //                allocator.add_to_heap(kend, end_addr);
-    //            }
-    //        }
-    //    }
-    //})
+    PHYSMEM_ALLOCATOR.with_lock(|mut allocator|{
+        for entry in mmap {
+            write!(SERIAL1, "\nChecking PMem entry {:?}", entry);
+            unsafe {
+                if !entry.is_for_general_use() { continue; }
+                write!(SERIAL1, "\n\tEntry is for general use.");
+                let start_addr: usize = entry.base_addr.try_into().unwrap();
+                let end_addr: usize = (entry.base_addr + entry.length).try_into().unwrap();
+                write!(SERIAL1, "\n\tRange: [{:x},{:x})", start_addr, end_addr);
+                if start_addr >= kend {
+                    // after the kernel
+                    write!(SERIAL1, "\n\tAfter the kernel");
+                    write!(SERIAL1, "\n\tadd_memory({:x},{:x})", start_addr, end_addr);
+                    allocator.add_memory(start_addr as *const u8, end_addr as *const u8);
+                } else if end_addr > kend {
+                    // intersecting the kernel
+                    write!(SERIAL1, "\n\tIntersects the kernel");
+                    write!(SERIAL1, "\n\tadd_memory({:x},{:x})", kend, end_addr);
+                    allocator.add_memory(kend as *const u8, end_addr as *const u8);
+                }
+            }
+        }
+        write!(SERIAL1, "\n\nResult:{:x?}", allocator);
+    })
 }
 
 // Return the size block to allocate for the given layout, assuming that the given block is aligned by itself (which is the case for allocations).

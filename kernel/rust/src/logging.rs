a4588d559b825fe04c0dcc12912790c2f40a48ab
Its.Annie <ctrlaltcs@gmail.com>
2024-08-04 16:20:09 +0100

    Disabled constant 'allocated X', 'deallocated X' messages from the physical memory allocator

--- kernel/rust/src/logging.rs
+++ kernel/rust/src/logging.rs
@@ -82,7 +82,7 @@ pub mod contexts {
       def_context!(MEMORY_PHYSICAL, MEMORY);
         def_context!(MEMORY_PHYSICAL_BUDDIES, MEMORY_PHYSICAL, Warning);
         def_context!(MEMORY_PHYSICAL_RAMMAP, MEMORY_PHYSICAL);
-        def_context!(MEMORY_PHYSICAL_ALLOCATOR, MEMORY_PHYSICAL);
+        def_context!(MEMORY_PHYSICAL_ALLOCATOR, MEMORY_PHYSICAL, Warning);
       def_context!(MEMORY_ALLOCUTIL, MEMORY, Debug);
     def_context!(FEATURE_FLAGS, ROOT, Info);
 }

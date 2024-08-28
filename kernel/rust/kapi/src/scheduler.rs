d51ea12e302674c4ccc06f4d69296b63c5f0fb4e
Its.Annie <ctrlaltcs@gmail.com>
2024-08-28 20:33:05 +0100

    eh

--- kernel/rust/kapi/src/scheduler.rs
+++ kernel/rust/kapi/src/scheduler.rs
@@ -1,7 +1,7 @@
 
 /* The scheduler command given to _cs_push is then passed over to the scheduler.
     It is used to tell the scheduler what to do with the task that just finished. */
-#[repr(C)]
+#[repr(u8)]
 pub enum SchedulerCommand {
     /// Push the current task back to the run_queue, and run it again once it's time
     PushBack,

1b170995a7cdddf0c206b8b2906b9c54001284af
Its.Annie <ctrlaltcs@gmail.com>
2024-09-13 20:27:17 +0100

    Tuples can now be marshalled

--- libsyscalls/src/marshal.rs
+++ libsyscalls/src/marshal.rs
@@ -1,5 +1,6 @@
 use crate::safety::{SyscallFFISafe,SyscallFFIMarshallable};
 
+// == ENUMS ==
 /// Define an enum that is Syscall FFI-compatible, with an automatic implementation of SyscallFFIMarshallable.
 /// (for convenience this also derives Clone,Copy,Debug as these are all applicable to any int-backed C-style enum)
 /// Syntax: [pub] enum(<integer type>) <name> { [variants with explicit discriminants...] }
@@ -49,6 +50,7 @@ ffi_enum! {
     }
 }
 
+//  == STRUCTS ==
 macro_rules! ffi_struct {
     (@impl_ffi for $name:ident, $vis:vis, $(($($repr:ident),+))?, ; $($ivis:vis $iname:ident: $itype:ty),+) => {
         // SAFETY: As the struct is repr(C) and all items are robust, the struct itself can be considered robust
@@ -131,6 +133,59 @@ ffi_struct! {
     }
 }
 
+//  == TUPLES ==
+// Tuples are serialized as an equivalent ordered marshallable struct
+// (note: only implemented for tuples up to arity 12)
+macro_rules! impl_tuple_marshal {
+    ($typename:ident, $($tparam:ident),+) => {
+        #[allow(dead_code)]
+        #[allow(non_snake_case)]
+        #[repr(C)]
+        pub struct $typename<$($tparam: $crate::safety::SyscallFFIMarshallable),+> {
+            $($tparam: $tparam::As),+
+        }
+        // SAFETY: As $typename contains the marshalled version of each type (::As), which is required to be safe, this struct is safe as well
+        unsafe impl<$($tparam: $crate::safety::SyscallFFIMarshallable),+> $crate::safety::SyscallFFISafe for $typename<$($tparam),+> {}
+        
+        impl<$($tparam: $crate::safety::SyscallFFIMarshallable),+> $crate::safety::SyscallFFIMarshallable for ($($tparam,)+) {
+            type As = $typename<$($tparam),+>;
+            fn marshall(value: Self) -> Self::As {
+                #[allow(non_snake_case)]
+                let ($($tparam,)+) = value;
+                $typename {$(
+                    $tparam: $crate::safety::SyscallFFIMarshallable::marshall($tparam),
+                )+}
+            }
+            fn demarshall(value: Self::As) -> Option<Self> {
+                #[allow(non_snake_case)]
+                let $typename { $($tparam,)+ } = value;
+                Some(($(
+                    $crate::safety::SyscallFFIMarshallable::demarshall($tparam)?,
+                )+))
+            }
+        }
+    }
+}
+impl_tuple_marshal!(FFI1Tuple, A);
+impl_tuple_marshal!(FFI2Tuple, A, B);
+impl_tuple_marshal!(FFI3Tuple, A, B, C);
+impl_tuple_marshal!(FFI4Tuple, A, B, C, D);
+impl_tuple_marshal!(FFI5Tuple, A, B, C, D, E);
+impl_tuple_marshal!(FFI6Tuple, A, B, C, D, E, F);
+impl_tuple_marshal!(FFI7Tuple, A, B, C, D, E, F, G);
+impl_tuple_marshal!(FFI8Tuple, A, B, C, D, E, F, G, H);
+impl_tuple_marshal!(FFI9Tuple, A, B, C, D, E, F, G, H, I);
+impl_tuple_marshal!(FFI10Tuple, A, B, C, D, E, F, G, H, I, J);
+impl_tuple_marshal!(FFI11Tuple, A, B, C, D, E, F, G, H, I, J, K);
+impl_tuple_marshal!(FFI12Tuple, A, B, C, D, E, F, G, H, I, J, K, L);
+
+const _: () = {
+    crate::safety::assert_ffi_marshallable::<(u32,u32)>();
+    crate::safety::assert_ffi_marshallable::<(u32,bool)>();
+    crate::safety::assert_ffi_marshallable::<(u32,(u32,u32,bool))>();
+};
+
+//  == UTIL TYPE ==
 #[repr(transparent)]
 pub struct FFIMarshalled<T:SyscallFFIMarshallable>(T::As);
 unsafe impl<T:SyscallFFIMarshallable> SyscallFFISafe for FFIMarshalled<T> {}

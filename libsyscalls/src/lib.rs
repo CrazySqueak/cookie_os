7ee6427b0e5abd683a0c655f9594f8df5f0df61d
Its.Annie <ctrlaltcs@gmail.com>
2024-10-24 18:35:22 +0100

    screw all this i'm gonna rebuild it from the ground up

--- libsyscalls/src/lib.rs
+++ libsyscalls/src/lib.rs
@@ -1,7 +1,1 @@
 #![no_std]
-
-mod syscore;
-pub mod syscalls;
-
-#[cfg_attr(target_arch = "x86_64",path="abi/x86_64.rs")]
-mod abi;
--- libsyscalls/src/syscore/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod safety;
-pub mod marshal;
-pub mod syscalls;
